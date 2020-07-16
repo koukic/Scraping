dcb41fbf518e5f623cac06cdb60de2ed58061e26
中條航紀 <ck227200@gmail.com>
2020-07-16 16:29:57 +0900

    :+1:Add json

--- nokogiri_sample.rb
+++ nokogiri_sample.rb
@@ -4,6 +4,22 @@ html = File.open('pitnews.html', 'r') { |f| f.read }
 
 doc = Nokogiri::HTML.parse(html, nil, 'utf-8')
 
-nodes = doc.xpath('//h6')
+pitnews = []
 
-nodes.each { |node| pp node }
+doc.xpath('/html/body/main/section').each_with_index do |section, index|
+  next if index.zero?
+  contents = {category: nil, news: []}
+  contents[:category] = section.xpath('./h6').first.text
+
+  section.xpath('./div/div').each do |node|
+    title = node.xpath('./p/strong/a').first.text
+    url = node.xpath('./p/strong/a').first['href']
+
+    news = {title: title, url: url}
+    contents[:news] << news
+  end
+  pitnews << contents
+end
+
+require 'json'
+File.open('pitnews.json', 'w') {|file| file.write( {pitnews: pitnews}.to_json) }
