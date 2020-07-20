d64aee20cb733f2bb6f01ad8ecb717c783fb3e27
中條航紀 <ck227200@gmail.com>
2020-07-20 18:20:28 +0900

    :bug:refact

--- scrape.rb
+++ scrape.rb
@@ -1,4 +1,6 @@
 require 'net/http'
+require 'nokogiri'
+require 'json'
 
 def get_from(url)
   Net::HTTP.get(URI(url))
@@ -8,4 +10,21 @@ def write_file(path, text)
   File.open(path, 'w') { |file| file.write(text) }
 end
 
-write_file('pitnews.html', get_from('https://masayuki14.github.io/pit-news/'))
+def scrape_news(news)
+  {
+    title: news.xpath('./p/strong/a').first.text,
+    url: news.xpath('./p/strong/a').first['href']
+  }
+end
+
+def scrape_section(section)
+  {
+    category: section.xpath('./h6').first.text,
+    news:  section.xpath('./div/div').map { |node| scrape_news(node) }
+  }
+end
+
+html = File.read('pitnews.html')
+doc = Nokogiri::HTML.parse(html, nil, 'utf-8')
+pitnews = doc.xpath('/html/body/main/section[position() > 1]').map { |section| scrape_section(section)}
+write_file('pitnews.json', {pitnews: pitnews}.to_json)
