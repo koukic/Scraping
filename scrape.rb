1106c758329f8760034f3bf357aec06dabdeb339
中條航紀 <ck227200@gmail.com>
2020-07-26 18:22:47 +0900

    :+1:Add optionparse

--- scrape.rb
+++ scrape.rb
@@ -2,6 +2,22 @@ require 'net/http'
 require 'nokogiri'
 require 'json'
 
+require 'opparse'
+opt = OptionParser.new
+opt.on('--infile=VAL')
+opt.on('--outfile=VAL')
+opt.on('--category=VAL')
+
+params = {}
+opt.parse!(ARGV, into; params)
+
+if params[:infile] && params[:category]
+  puts 'Error: --infile と --category は同時に指定できません。'
+  exit(1)
+end
+
+
+
 def get_from(url)
   Net::HTTP.get(URI(url))
 end
@@ -22,6 +38,20 @@ def scrape_section(section)
     category: section.xpath('./h6').first.text,
     news:  section.xpath('./div/div').map { |node| scrape_news(node) }
   }
+end
+
+pitnews = doc
+  .xpath('/html/body/main/section[position() > 1]')
+  .map { |section| scrape_section(section) }
+
+
+if params[:infile]
+  html = File.read(params[:infile])
+else
+  url = 'https://masayuki14.github.io/pit-news/'
+  if params[:category]
+    url = url + '?category=' + params[:category]
+  end
 end
 
 html = File.read('pitnews.html')
